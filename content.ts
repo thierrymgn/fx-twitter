66194f27858163cb856b62a8c362426d164a8397
thierrymgn <maignan.thierry@outlook.fr>
2023-10-12 22:41:39 +0200

    add content logic

---
+++ content.ts
@@ -0,0 +1,13 @@
+document.addEventListener("copy", (e: ClipboardEvent) => {
+  const copiedText = window.getSelection()?.toString() || ""
+  console.log(copiedText)
+  if (isTwitterLink(copiedText)) {
+    const modifiedText = copiedText.replace(/x\.com/g, "fxtwitter.com")
+    e.clipboardData.setData("text/plain", modifiedText)
+    e.preventDefault()
+  }
+})
+
+function isTwitterLink(text: string): boolean {
+  return /x\.com/.test(text)
+}
